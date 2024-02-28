f09be9673f19196478ef4e1a47dd774805e96481
dvhar <dh22@protonmail.com>
2024-02-27 21:15:40 -0600

    improve launcher script

--- run.sh
+++ run.sh
@@ -1,7 +1,46 @@
-#!/bin/zsh
+#!/bin/bash
 
-debuglog=/tmp/pikeru.log
-cd `dirname $0`
-echo "$*" > $debuglog
-echo '------' >> $debuglog
-./pikeru.py $@ 2>&1 >> $debuglog
+# comment out or set to your venv
+. ~/stuff/v1/bin/activate
+
+
+TITLE="File Picker"
+TYPE="file"
+DIR="$(pwd)"
+MIME_LIST=""
+
+while getopts "e:t:k:p:i:" opt; do
+  case $opt in
+    e)
+      PARENT="$OPTARG"
+      ;;
+    t)
+      TITLE="$OPTARG"
+      ;;
+    k)
+      TYPE="$OPTARG"
+      ;;
+    p)
+      DIR="$OPTARG"
+      ;;
+    i)
+      MIME_LIST="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [[ ! "$TYPE" =~ ^(file|files|dir|save)$ ]]; then
+  echo "Error: Invalid type flag value (-k). It should be one of [file files dir save]." >&2
+  exit 1
+fi
+
+python ./pikeru.py \
+  --title "${TITLE}" \
+  --type "${TYPE}" \
+  --path "${DIR}" \
+  --mime_list "${MIME_LIST[@]:-}" \
+  --parent "${PARENT:-}"
