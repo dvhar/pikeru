453bcc79d8d9ae39ecc31ffa0ea61f3287a7eced
dvhar <dh@protonmail.com>
2024-03-12 06:20:33 -0500

    make installation easier

--- run.sh
+++ run.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+# set environment variable PK_VENV=/path/to/venv or put 'venv' in this directory
+# set debug=1 if you're having trouble getting it working. It logs to /tmp/pk.log
 
-venv=~/stuff/v1/bin/activate
-debug=1
+debug=0
+
+venv=$PK_VENV
+[ -z "$venv" ] && venv=venv
+
+DIR="$(pwd)"
+if [[ "$(readlink -f "$0")" != "$0" ]]; then
+    cd "$(dirname "$(readlink -f "$0")")"
+else
+    cd "$(dirname "$0")"
+fi
 
 TITLE="File Picker"
 MODE="file"
-DIR="$(pwd)"
 MIME_LIST=""
 
-[ -z $debug ] || echo "launcher args: $*" > /tmp/pkerr
+[ $debug = 1 ] && echo "launcher args: $*" > /tmp/pk.log
 while getopts "e:t:m:p:i:" opt; do
   case $opt in
     e)
@@ -27,18 +37,27 @@ while getopts "e:t:m:p:i:" opt; do
       MIME_LIST="$OPTARG"
       ;;
     \?)
-      [ -z $debug ] || echo "Invalid option: -$opt -$OPTARG" >> /tmp/pkerr
+      echo "Invalid option: -$opt -$OPTARG" >> /tmp/pk.log
       exit 1
       ;;
   esac
 done
 
 if [[ ! "$MODE" =~ ^(file|files|dir|save)$ ]]; then
-  [ -z $debug ] || echo "Error: Invalid mode flag value (-m). It should be one of [file files dir save]." >> /tmp/pkerr
+  echo "Error: Invalid mode flag value (-m). It should be one of [file files dir save]." >> /tmp/pk.log
   exit 1
 fi
 
-[ -z "$venv" ] || . ~/stuff/v1/bin/activate
+if [ ! -f "$venv/bin/activate" ]; then
+cat << EOF > /dev/stderr
+You may need to set up a venv. Put a venv in this directory or set environment variable PK_VENV to /path/to/venv.
+Try this:
+python -m venv $venv
+pip3 install -r requirements.txt
+EOF
+else
+	. $venv/bin/activate
+fi
 
 #not using title flag but you can if you want
 cmd="python ./pikeru.py \
@@ -47,6 +66,5 @@ cmd="python ./pikeru.py \
 	--title '${TITLE}' \
 	--mime_list '${MIME_LIST[@]:-}' \
 	--parent '${PARENT:-}'" 
-[ -z $debug ] || echo "cmd: $cmd" >> /tmp/pkerr
-cd `dirname $0`
+[ $debug = 1 ] || echo "cmd: $cmd" >> /tmp/pk.log
 eval $cmd
