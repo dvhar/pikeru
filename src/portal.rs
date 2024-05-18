5a936c942346acacf0d92bda01096967b4074de3
dvhar <dh22@protonmail.com>
2024-05-17 20:27:32 -0500

    rust portal works

--- src/portal.rs
+++ src/portal.rs
@@ -10,50 +10,52 @@ use std::collections::HashMap;
 
 struct FilePicker;
 
+fn select_file(dir: bool, multi: bool, save: bool, path: &str) -> (u32, HashMap<String, OwnedValue>) {
+    let dir   = if dir   { 1 } else { 0 };
+    let multi = if multi { 1 } else { 0 };
+    let save  = if save  { 1 } else { 0 };
+    let cmd = format!("/usr/local/share/xdg-desktop-portal-pikeru/pikeru-wrapper.sh {} {} {} {}",
+                      multi, dir, save, path);
+    let output = match std::process::Command::new("sh").arg("-c").arg(cmd).output() {
+        Ok(out) => unsafe { std::str::from_utf8_unchecked(&out.stdout).to_owned() },
+        Err(e) => {eprintln!("Process error: {}", e); "".to_owned()},
+    };
+    let arr = output.lines().map(|line|format!("file://{}",line)).collect::<Vec<_>>();
+    let mut ret = HashMap::new();
+    let status = if arr.is_empty() { 1 } else {
+        let uris = Value::from(arr).try_to_owned().unwrap();
+        ret.insert("uris".to_string(), uris);
+        0
+    };
+    (status, ret)
+}
+
 #[interface(name = "org.freedesktop.impl.portal.FileChooser")]
 impl FilePicker {
     fn open_file(&self, _ob: zbus::zvariant::ObjectPath<'_>, _caller: &str, _parent: &str,
                  _title: &str, options: HashMap<&str, Value<'_>>) -> (u32, HashMap<String, OwnedValue>) {
-        eprint!("Open file");
         let multi = options.get("multiple").unwrap_or(&Value::Bool(false));
         let dir = options.get("directory").unwrap_or(&Value::Bool(false));
         let dir = match dir {
-            &Value::Bool(true) => 1,
-            &Value::Bool(false) => 0,
-            _ => { eprintln!("DIR type error"); 0},
+            &Value::Bool(b) => b,
+            _ => { eprintln!("DIR type error"); false},
         };
         let multi = match multi {
-            &Value::Bool(true) => 1,
-            &Value::Bool(false) => 0,
-            _ => { eprintln!("MULTI type error"); 0},
+            &Value::Bool(b) => b,
+            _ => { eprintln!("MULTI type error"); false},
         };
-        let cmd = format!("/usr/local/share/xdg-desktop-portal-pikeru/pikeru-wrapper.sh {} {} 0 /home/d",
-                          multi, dir);
-        let output = match std::process::Command::new("sh").arg("-c").arg(cmd).output() {
-            Ok(out) => unsafe { std::str::from_utf8_unchecked(&out.stdout).to_owned() },
-            Err(e) => {eprintln!("Process error: {}", e); "".to_owned()},
-        };
-        let arr = output.lines().map(|line|{
-            Value::from(format!("file://{}",line))
-        }).collect::<Vec<_>>();
-        let mut ret = HashMap::new();
-        let status = if arr.is_empty() { 1 } else {
-            let uris = Value::from(arr).try_to_owned().unwrap();
-            ret.insert("uris".to_string(), uris);
-            0
-        };
-        eprintln!("RET:({},\n{:#?})", status, ret);
-        (status, ret)
+        select_file(multi, dir, false, "/home/d")
     }
 
     fn save_file(&self, _ob: zbus::zvariant::ObjectPath<'_>, _caller: &str, _parent: &str,
                  _title: &str, options: HashMap<&str, Value<'_>>) -> (u32, HashMap<String, OwnedValue>) {
         eprint!("Save options:{:#?}", options);
-        let _fname = match options.get("current_name") {
-            Some(val) => val.to_string(),
+        let fname = match options.get("current_name") {
+            Some(s) => s.to_string(),
             None => "download".to_string(),
         };
-        (0, HashMap::default())
+        let path = format!("/home/d/{}", fname);
+        select_file(false, false, true, path.as_str())
     }
 }
 
