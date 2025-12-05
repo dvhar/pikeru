980d35d66dac442e4094813608b4c0853e425e44
dvhar <dh22@protonmail.com>
2025-12-04 19:29:17 -0600

    1.11

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,7 @@ fn cli(flags: &getopts::Matches) {
         std::process::exit(0);
     }
     if flags.opt_present("v") {
-        println!("1.10");
+        println!("1.11");
         std::process::exit(0);
     }
     let cmd = if flags.opt_present("d") {
